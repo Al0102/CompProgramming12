83e36fdcf5f81750cad500f5227389aeb2b5aab4
Al0102 (Alex Lu) <mealex2007@gmail.com>
2024-10-07 11:40:22 -0700

    removed duplicate file

--- assignments/a1_Lu_Alex_.py
+++
@@ -1,2 +0,0 @@
-def convert(a): return a.replace(":)", "😊").replace(":(", "😢") 
-print(input("To lowercase: ").lower() + "\n" + input("Give attitude: ").replace(" ", "...") + "\n" + convert(input("Emotify: ")) + "\n" + str(int(input("Relative energy of this mass: ")) * (3*10**8)**2) )
